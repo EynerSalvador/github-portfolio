2c8b2c38980fdf96350502709a7b882d8df7bc13
EynerSalvador <168598767+EynerSalvador@users.noreply.github.com>
2025-04-23 15:14:24 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,7 @@
+from app.db.database import db
 
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    skills = db.Column(db.JSON)
+    projects = db.Column(db.JSON)
