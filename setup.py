f849c45c6853759e60bd0ec716eebe077372262d
EynerSalvador <168598767+EynerSalvador@users.noreply.github.com>
2025-04-23 12:12:26 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="github-portfolio",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "flask==3.0.2",
+        "flask-sqlalchemy==3.1.1",
+        "pytest==8.3.5"
+    ],
+)
