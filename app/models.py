c1b580fa4c49b75c636be8868d5535ca54ae0578
EynerSalvador <168598767+EynerSalvador@users.noreply.github.com>
2025-04-23 14:33:21 -0500

    Delete app/models.py

--- app/models.py
+++
@@ -1,8 +0,0 @@
-from db.database import db
-
-class Profile(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), unique=True)
-    skills = db.Column(db.JSON)  # Ej: ["Python", "HTML"]
-    projects = db.Column(db.JSON)  # Ej: [{"name": "Portafolio", "url": "/"}]
