9df714d6174c864995c503c73b90119bb2ee4998
EynerSalvador <168598767+EynerSalvador@users.noreply.github.com>
2025-04-23 15:13:44 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,7 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from app.db.database import init_db
 
 def create_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///portfolio.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    
-    db.init_app(app)
-    
-    with app.app_context():
-        db.create_all()
-    
+    app = Flask(__name__, static_folder='../static', template_folder='../templates')
+    init_db(app)
     return app
