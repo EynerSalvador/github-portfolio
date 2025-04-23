acde55b788566ff5e475d137ee0bdabf9590a697
Wilder-Arredondo <48569015@continental.edu.pe>
2025-04-22 22:22:37 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()  # Inicializa SQLAlchemy aquí
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../db/database.db'
+db = SQLAlchemy(app)
