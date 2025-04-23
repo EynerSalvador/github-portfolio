d79bccd48bf8b99dac750d7d70c56f454c1cf259
Wilder-Arredondo <48569015@continental.edu.pe>
2025-04-22 22:12:26 -0500

    Update test_models.py

--- app/tests/test_models.py
+++ app/tests/test_models.py
@@ -1,1 +1,10 @@
 
+import pytest
+from app.models import db, Post
+
+def test_create_post():
+    """Prueba que se pueda crear un Post en la DB."""
+    post = Post(title="TDD en Python", content="Ejemplo de prueba")
+    db.session.add(post)
+    db.session.commit()
+    assert post.id is not None  # Verifica que se guardó en la DB
