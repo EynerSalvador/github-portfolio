4e15cbbcb164fe61bcdb50d1c5e4877b007df6ad
EynerSalvador <168598767+EynerSalvador@users.noreply.github.com>
2025-04-23 15:29:56 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,18 @@
 from flask import Flask
-from app.db.database import init_db
+from app.db.database import db
 
 def create_app():
-    app = Flask(__name__, static_folder='../static', template_folder='../templates')
-    init_db(app)
+    app = Flask(__name__)
+    
+    # Configuración de la DB (SQLite)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///portfolio.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    # Inicialización
+    db.init_app(app)
+    
+    # Crear tablas (esto puede moverse a un script aparte si prefieres)
+    with app.app_context():
+        db.create_all()
+    
     return app
