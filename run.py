f471a0a00bc852a5f1fbf8567579bf6a8e12ca48
EynerSalvador <168598767+EynerSalvador@users.noreply.github.com>
2025-04-23 16:44:19 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-# run.py
-import os
 import sys
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from pathlib import Path
+
+# Añade el directorio raíz al path
+sys.path.append(str(Path(__file__).parent))
 
 from app import create_app
 
