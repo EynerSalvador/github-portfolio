065dd99c74336fcf30bbae6b4668b10580f6dd35
Wilder-Arredondo <48569015@continental.edu.pe>
2025-04-22 22:23:00 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,11 +1,6 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from app import db  # Importa db desde app/
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text)
-
-    def to_dict(self):
-        return {"id": self.id, "title": self.title, "content": self.content}
