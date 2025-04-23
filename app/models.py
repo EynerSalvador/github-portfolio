5f83ce0493d32b7e842f80aaaa14f5fb5d4788c9
EynerSalvador <168598767+EynerSalvador@users.noreply.github.com>
2025-04-23 09:00:39 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
-from app import db  # Importa db desde app/
+from db.database import db
 
-class Post(db.Model):
+class Profile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True)
+    skills = db.Column(db.JSON)  # Ej: ["Python", "HTML"]
+    projects = db.Column(db.JSON)  # Ej: [{"name": "Portafolio", "url": "/"}]
