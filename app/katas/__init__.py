3d7d62ed961982397c8b7a62e540535bc13518eb
EynerSalvador <168598767+EynerSalvador@users.noreply.github.com>
2025-04-23 12:27:17 -0500

    Update __init__.py

--- app/katas/__init__.py
+++ app/katas/__init__.py
@@ -1,7 +1,1 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../db/database.db'
-db = SQLAlchemy(app)
 
