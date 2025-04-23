e0852e4804da3f8226199255d0c0bea8f0a4079b
EynerSalvador <168598767+EynerSalvador@users.noreply.github.com>
2025-04-23 12:04:44 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+# Configuración de la base de datos (si usas SQLAlchemy)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///portfolio.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Importa rutas o modelos al final para evitar dependencias circulares
+from app import models
