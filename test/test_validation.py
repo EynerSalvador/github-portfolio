08e2dafc86db70253d58306ace8afb9b74abcc1c
EynerSalvador <168598767+EynerSalvador@users.noreply.github.com>
2025-04-23 12:29:14 -0500

    Delete test directory

--- test/test_validation.py
+++
@@ -1,5 +0,0 @@
-from app.katas.validation import validate_skill
-
-def test_validate_skill():
-    assert validate_skill("Python", ["Python", "Java"]) is True
-    assert validate_skill("Ruby", ["Python"]) is False
